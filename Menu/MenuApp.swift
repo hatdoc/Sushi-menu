52d8939a980871359a8a51546cd7d8d365d9ffa3
Jed <hatdoc@naver.com>
2025-04-19 19:58:20 +0800

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Jed Park on 19/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenuTests/MenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTests.swift
+//  MenuTests
+//
+//  Created by Jed Park on 19/4/2025.
+//
+
+import Testing
+@testable import Menu
+
+struct MenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
