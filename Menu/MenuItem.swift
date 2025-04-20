54d579dc0d60e541aa825824a734cc27d91b88bf
Jed <hatdoc@naver.com>
2025-04-20 15:58:25 +0800

    Create new file called MenuItem and apply in main view

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,16 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Jed Park on 20/4/2025.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable {
+    
+    var id: UUID = UUID()
+    var name:String
+    var price:String
+    var imageName:String
+}
